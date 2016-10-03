c2ae064207b69a918bab860f26789646bca6b68c
prgdanny90 <prgdanny90@gmail.com>
2016-10-03 21:27:19 +0900

    test2_webapp2

--- index.py
+++ index.py
@@ -1,16 +1,8 @@
-from google.appengine.ext import webapp
-from google.appengine.ext.webapp.util import run_wsgi_app
-
-class MainPage(webapp.RequestHandler):
+import webapp2
+class ParseHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.out.write('Hello, webapp World!')
+        self.response.out.write("ok")
 
-application = webapp.WSGIApplication([('/', MainPage)],
-                                     debug=True)
-
-def main():
-    run_wsgi_app(application)
-
-if __name__ == "__main__":
-    main()
+app = webapp2.WSGIApplication([
+    ('/init', ParseHandler)
+], debug=True)
